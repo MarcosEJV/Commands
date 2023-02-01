f1c697efe7aa246dc7a5da58e4dc4b0c331da3c0
Marcos E Jaso-Vera <mjasove1@jh.edu>
2023-02-01 17:37:57 -0500

    Initial commands I added on 020123.

---
+++ Command-list.R
@@ -0,0 +1,16 @@
+#List of commands and a little informative comment about them.
+
+print("text")   #Prints the text or variable.
+
+x <-  value or data   #Assigns a value to the variable "x"
+
+data.frame( column1 = c(x,y,z),    #Generates a dataframe
+            column2 = c(a,b,c),
+            column3 = c(1,2,3)
+)
+
+matrix(c(1:10),    #Generates a matrix
+       nrow = 2,
+       ncol = 5
+)
+
