c3ce9f20c05d443364524dcced68e7b5e06adaa3
MarcosEJV <mejasov@gmail.com>
2023-02-01 20:44:28 -0500

    Commands added from notebook. Graphic commands are still missing. 020123.

--- Command-list.R
+++ Command-list.R
@@ -7,10 +7,74 @@ x <-  value or data   #Assigns a value to the variable "x"
 data.frame( column1 = c(x,y,z),    #Generates a dataframe
             column2 = c(a,b,c),
             column3 = c(1,2,3)
+            row.names = c(a,b,c)   #This adds names to each row in an extra column.
 )
 
 matrix(c(1:10),    #Generates a matrix
        nrow = 2,
        ncol = 5
 )
+
+getwd()         #This gives you the current directory.
+
+setwd("/location")  #This sets a directory as the working directory.
+
+round(number,decimals)  #Round a number, adding decimals acording to the indication.
+
+c(data)         #This generates vectors.
+
+"dataframename"[x,y]  #To see the data in a specific cell of a dataframe.
+
+"dataframename"[,c(1,3)]  #All rows, just column 1 and 3 of a dataframe.
+
+head(dataframe)   #Shows the title of the dataframe table.
+
+Dataframe[Dataframe$Column|Condition]   #Gives you the specific data of a column that complies with specific conditions.
+
+tail()    #Shows the last row of a table.
+
+Summary()   #Shows a summary of a table.
+
+If(argument){ event 1,    #Conditional for order execution.
+              event 2}
+              else { event 3,   #Not needed for "If" conditional.
+                      event 4
+              }
+
+for("changes"in"values") { action/consequence   #Cycles.
+}
+
+variable <- read.table()  #Builds dataframes from table files.
+
+names("table")  #Print first column.
+head("table")   #Print first line.
+tail("table")   #Print last line.
+nrow("table")   #Cound number of rows.
+
+"Table"[rowSums(countTable)>0,]   #Deletes rows without data.
+
+write.table("table","table.txt",quote = FALSE, sep = "\t")  #Saves a table as text file
+                            #the quote eliminates quotation marks.
+                            #sep argument indicates separation using tabulation.
+
+ddply(.data = table,        #It gives you the means of column 2 using column 1 as a filter.
+      .variables = 'column',
+      .fun = function(x) mean(x$column2)
+      )
+
+[which.max(table[,2]),]     #It tells you the highest value taking out the NA.
+
+select(dataframe,column1,column2,...)   #Generates a new dataframe selecting specific columns.
+
+filter(dataframe,row1,row2,...)         #As select but for rows.
+
+summarize()     #Turns a dataframe in only 1 row.
+
+group_by()      #Turns a dataframe intro groups depending on specific conditions.
+
+is.na()         #Filters NA values.
+
+mutate()        #Add new columns to a dataframe.
+
+count()         #Counts data.
 
---
+++ Packages-list.R
@@ -0,0 +1,7 @@
+#List of packages.
+
+Library(plyr)   #Data manipulation.
+
+Library(dplyr)  #Same as plyr but it allows pipeline use. %>%
+
+Library(ggplot2)    #For figure generation.
