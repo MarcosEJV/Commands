a3a5185fc0dc98c71d58892e75bfbb877721216e
MarcosEJV <mejasov@gmail.com>
2023-02-04 19:04:34 -0500

    Finished 2nd week classes from coursera R programming basic course. 020423

--- Command-list.R
+++ Command-list.R
@@ -29,17 +29,18 @@ c(data)         #This generates vectors.
 
 head(dataframe)   #Shows the title of the dataframe table.
 
-Dataframe[Dataframe$Column|Condition]   #Gives you the specific data of a column that complies with specific conditions.
+Dataframe[Dataframe$Column|Condition]   #Gives you the specific data of a column- 
+                                        #that complies with specific conditions.
 
 tail()    #Shows the last row of a table.
 
 Summary()   #Shows a summary of a table.
 
 If(argument){ event 1,    #Conditional for order execution.
-              event 2}
-              else { event 3,   #Not needed for "If" conditional.
-                      event 4
-              }
+  event 2}
+else { event 3,   #Not needed for "If" conditional.
+  event 4
+}
 
 for("changes"in"values") { action/consequence   #Cycles.
 }
@@ -54,13 +55,15 @@ nrow("table")   #Cound number of rows.
 "Table"[rowSums(countTable)>0,]   #Deletes rows without data.
 
 write.table("table","table.txt",quote = FALSE, sep = "\t")  #Saves a table as text file
-                            #the quote eliminates quotation marks.
-                            #sep argument indicates separation using tabulation.
+#the quote eliminates quotation marks.
+#sep argument indicates separation using tabulation.
+
+#Next commands use plyr package.
 
 ddply(.data = table,        #It gives you the means of column 2 using column 1 as a filter.
       .variables = 'column',
       .fun = function(x) mean(x$column2)
-      )
+)
 
 [which.max(table[,2]),]     #It tells you the highest value taking out the NA.
 
@@ -77,4 +80,28 @@ is.na()         #Filters NA values.
 mutate()        #Add new columns to a dataframe.
 
 count()         #Counts data.
+
+#End of plyr package known commands.
+
+search()        #Shows the search list R is using to assing values to variables and functions.
+
+ls(environment())   #It tells you what is inside a function environment.
+
+as.Date("date")       #Function for coercing date as a character as follows:
+
+x <- as.Date("1989-07-14")
+unclass(x)
+
+weekdays()      #Function for day of the week.
+
+months()        #Function for month name.
+
+quarters()      #Function for quarted number i.e. "Q1"
+
+Sys.time()      #System time.
+
+strptime(datestring, "%B %d, %y %H:%M")   #Function to acquire the time from different format.
+        #B-month d-day y-year in 4 digits H-hour m-minute
+
+as.POSIXct("date", tz = "EST")  #To manipulate time zone data. tz = time zone.
 
