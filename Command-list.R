3b9d33f58ad16901f32214dd817166a2c002e2a3
Marcos <123852633+MarcosEJV@users.noreply.github.com>
2025-05-20 12:08:02 -0400

    Update Command-list.R
    Added commands for exploring and getting information from MySQL and HDF5 files.

--- Command-list.R
+++ Command-list.R
@@ -1,7 +1,6 @@
 #List of commands and a little informative comment about them.
 
 print("text")   #Prints the text or variable.
-
 x <-  value or data   #Assigns a value to the variable "x"
 
 data.frame( column1 = c(x,y,z),    #Generates a dataframe
@@ -16,24 +15,17 @@ matrix(c(1:10),    #Generates a matrix
 )
 
 getwd()         #This gives you the current directory.
-
 setwd("/location")  #This sets a directory as the working directory.
-
 round(number,decimals)  #Round a number, adding decimals acording to the indication.
-
 c(data)         #This generates vectors.
-
 "dataframename"[x,y]  #To see the data in a specific cell of a dataframe.
-
 "dataframename"[,c(1,3)]  #All rows, just column 1 and 3 of a dataframe.
-
 head(dataframe)   #Shows the title of the dataframe table.
 
 Dataframe[Dataframe$Column|Condition]   #Gives you the specific data of a column- 
                                         #that complies with specific conditions.
 
 Summary()   #Shows a summary of a table.
-
 dim()       #To look at the structure of a dataframe. The output is the number of rows and columns.
 
 If(argument){ event 1,    #Conditional for order execution.
@@ -56,12 +48,12 @@ tail("table")   #Print last row.
 nrow("table")   #Count number of rows.
 
 "Table"[rowSums(countTable)>0,]   #Deletes rows without data.
-
 table("dataframe"$Column)   #To know how many times each value actually occurs.
 
 write.table("table","table.txt",quote = FALSE, sep = "\t")  #Saves a table as text file
 #the quote eliminates quotation marks.
 #sep argument indicates separation using tabulation.
+
 write.xlsx()    #Write an Excel. Requires Library xlsx.
 
                   ------------XML files---------------
@@ -79,6 +71,28 @@ name(x$owner)   #Look for the title of the "owner" column inside the JSON file.
 toJSON(x,pretty=SURE)            #To convert a dataframe into JSON. pretty makes easier to read the file.
 cat(x)          #To print the JSON file.
 
+                 ------------MySQL Package---------------
+
+dbConnect(MySQL(), user = "username", db = "gene", host = "http://website")            #To download the data from the internet. db is used when you already know the gene you want to subset.
+dbGetQuery(x, "show databases;") #Goes to the database x and perform the command to show databases.
+dbGetQuery(x, "select count (*) from "table"")            #To know how many elements are in the table.
+dbListTables(x) #It gives the number of tables in the database.
+dbListFields(x, "Table name")    #Look at the fields in a particular table (fields are the columns).
+dbReadTable(x, "Table name")     #Gives you the dataframe.
+dbSendQuery(x, "select * from "table" where "column" between 1 and 3")            #It gives you all the variables of the table where the value of the "column" is between 1 and 3".
+fetch(query, n=10)          #Shows the top 10 in the query.
+dbClearResult(query)        #To clean the query when you are done.
+dbDisconnect(x) #To disconnect from the database. REMEMBER TO DO THIS EVERYTIME YOU FINISHED.
+            #NEVER COMMIT CHANGES TO THE DATABASE, JUST USE IT FOR DOWNLOAD.
+
+                 ------------HDF5 Package----------------
+
+h5createfile("name.h5")     #To create an HDF5 file.
+h5createGroup("file.h5", "group name")            #To create a group inside an existing HDF5 file.
+h5ls("file.h5")  #Similar to ls, but for HDF5 files.
+h5write(x, "file.h5", "group/object")             #To insert data in the HDF5 file.
+h5read("file.h5", "group/object")                 #To read the file.
+
                  ------------dplyr Package---------------
 
 ddply(.data = table,        #It gives you the means of column 2 using column 1 as a filter.
@@ -89,63 +103,44 @@ ddply(.data = table,        #It gives you the means of column 2 using column 1 a
 [which.max(table[,2]),]     #It tells you the highest value taking out the NA.
 
 select(dataframe,column1,column2,...)   #Generates a new dataframe selecting specific columns.
-
 filter(dataframe,row1,row2,...)         #As select but for rows.
-
 summarize()     #Turns a dataframe in only 1 row.
-
 group_by()      #Turns a dataframe intro groups depending on specific conditions.
-
 is.na()         #Filters NA values.
-
 na.omit()       #Erase NA rows.
-
 mutate()        #Add new columns to a dataframe.
-
 count()         #Counts data.
+%>%             #For pipelines.
 
                 -----------------data.table Package----------------
 
 table()         #It gives you all the information of the tables in the environment.
-
 x[]             #Subsetting tables.
-
 x[,function()]  #Perform function to specific columns and variables in the table.
-
 x[,title:"new information"]   #Adding columns to an existing table. You can use formulas using other columns. You can also use logic tests.
-
 x[, .N, by=y]   #.N is a special variable that counts the number of times a value or variable appears in a table.
-
 fread(file)     #Fast reading tables. Much faster than read.table().
 
                 ---------------------------------------------------
 
 search()        #Shows the search list R is using to assing values to variables and functions.
-
 ls(environment())   #It tells you what is inside a function environment.
-
 as.Date("date")       #Function for coercing date as a character as follows:
 
 x <- as.Date("1989-07-14")
 unclass(x)
 
 weekdays()      #Function for day of the week.
-
 months()        #Function for month name.
-
 quarters()      #Function for quarted number i.e. "Q1"
-
 system.time()   #Computes the time needed to execute and expression (in seconds). Returns a proc_time class object. user time: Time charged to the CPU. elapsed time: 'wall clock' time.
-
 Sys.time()      #System time.
 
 strptime(datestring, "%B %d, %y %H:%M")   #Function to acquire the time from different format.
         #B-month d-day y-year in 4 digits H-hour m-minute
 
 as.POSIXct("date", tz = "EST")  #To manipulate time zone data. tz = time zone.
-
 x != y          #Logical operator that tests if two values are not equal.
-
 &               #"And" operator. If one is FALSE, everything is FALSE.
   
 &&              #"And-and" operator, it evaluates only the corresponding values 1v1.
@@ -160,32 +155,21 @@ xor(argument1,argument2)  #"Exclusive or", if you have 1 TRUE argument, it retur
                           #If you have 2 FALSE or 2 TRUE, it returns FALSE.
 
 which()         #Indicates which values are TRUE in the variable.
-
 any()           #Returns a TRUE if there is any TRUE in the variable.
-
 all()           #Returns TRUE if all elements are TRUE.
-
 %%              #Remainer of a divison.
-  
 function(...)   #The 3 dots are call ellipsis and it means "indefinite number of arguments".
-  
 lapply(x,function,(...))        #Loop over a list and evaluate a function on each element.
-
 sapply()        #lapply() with a more simple output.
-
 apply(x,margin,function,...)         #Apply a function over the margins of an array. The margin is an integer vector which margins should be retained. i.e. apply(x,1,sum) <- rowSums(x)
-
 tapply(x, index, function,..., SIMPLIFY = TRUE)        #Apply a function over subsets of a vector. x is a vector. index is a factor or a list of factors.
-
 mapply(function,...,MoreArgs = NULL, SIMPLIFY = TRUE, USE.NAMES = TRUE)        #Multivariate version of lapply().
 
 split(x,f, drop = FALSE,...)          #Takes a vector or other objects and splits it into groups determined by a factor or list of factors. x is a vector, list or dataframe. f is a factor. frop says if empty factors
                                       #should be dropped.
        
 date()          #It prints the date of the system.
-
 file.exists("x")            #It verifies the presence of a file in the working directory.
-
 dir.create("x")             #Creates a directory in the current working directory.
 
 download.file(websiteinvariable, destfile = "directoryofdestination/filename.extension", method = "curl")            #For downloading files from the internet, 
@@ -194,43 +178,27 @@ download.file(websiteinvariable, destfile = "directoryofdestination/filename.ext
        ----------------- Statistics -------------------
 
 rnorm()         #Random numbers given a mean and STdev. r from random.
-
 dnorm()         #Normal probability (given mean and STdev) at a point (or vector of points). d from density.
-
 pnorm()         #Cumulative distribution function for a Normal distribution. p from probability.
-
 rpois()         #Random Poisson variates with a given rate.
-
 qnorm()         #Quantile function. q from quantile.
-
 set.seed()      #Random number seed. This function allows reproducibility when generating numbers.
-
 sample()        #Draw random samples from objects. if replace = TRUE -> the picked valued can be repeated.
 
        ----------------- For plotting (basic) ------------------
 
 plot(x, y, "type", main = "title", xlab = "x-axis", ylab = "y-axis", pch = number)            #type -> white dots are default, "l" is a line chart. pch = 16 for black circles. For more, look into ?plot.
-
 hist()          #Generates and histogram from a vector.
-
 boxplot()       #Bloxplot from a dataframe.
        
        ----------------- For debugging ---------------------
 
 traceback()     #Prints out the function call stack after an error.
-
 debug()         #Enters "debug" mode, step trhough execution one line at a time.
-
 browser()       #Suspend execution of a function wherever it is called.
-
 trace()         #Insert debugging code without modifying the function.
-
 recover()       #Modify the error behavior so tou can get the function call stack.
-
 str()           #Internal structure of an R object. Simillar to summary().
-
 Rprof()         #Starts the Profiler in R.
-
 SummaryRprof()  #Makes Rprof() readable. Keeps track of the function call stack and tabulates how much time is spend on each function. NEVER USE Rprof() and system.time() together.
-
 object.size()   #Splace in memory of an object.
