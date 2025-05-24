b8bafc592cb022f53d570ee16e30fb49b028cd38
Marcos <123852633+MarcosEJV@users.noreply.github.com>
2025-05-24 11:33:43 -0400

    Update Command-list.R
    Added commands for dplyr, plyr, Hmisc, and reshape2.
    Added commands for data frame manipulation.

--- Command-list.R
+++ Command-list.R
@@ -20,13 +20,21 @@ round(number,decimals)  #Round a number, adding decimals acording to the indicat
 c(data)         #This generates vectors.
 "dataframename"[x,y]  #To see the data in a specific cell of a dataframe.
 "dataframename"[,c(1,3)]  #All rows, just column 1 and 3 of a dataframe.
-head(dataframe)   #Shows the title of the dataframe table.
 
 Dataframe[Dataframe$Column|Condition]   #Gives you the specific data of a column- 
                                         #that complies with specific conditions.
 
 summary()   #Shows a summary of a table.
 dim()       #To look at the structure of a dataframe. The output is the number of rows and columns.
+abs()       #Absolute value.
+sqrt()      #Square root.
+ceiling()   #Round up.
+floor()     #Round down.
+round(x, digits = n)      #Rounds.
+cos(x) sin(x)
+log(x)
+log2(x) log10()
+exp(x)
 
 If(argument){ event 1,    #Conditional for order execution.
   event 2}
@@ -50,12 +58,30 @@ str("table")           #Internal structure of an R object. Simillar to summary()
 
 "Table"[rowSums(countTable)>0,]   #Deletes rows without data.
 table("dataframe"$Column)   #To know how many times each value actually occurs.
+table(data$"column", data$"column2")     #To build tables from data.
+sum(is.na(x$"column")) #How many NAs are in the data column.
+colSums(x)      #To get a sum from each column.
+table(x$"column" %in% "data")            #How many values in the column from x are in "data".
+x[x$"column" %in% "data"]   #Subsetting based on selection.
+xtabs(column1 ~ column2 + column3, data = x)                #Cross tabs the data to gove you a cress reference for data analysis (see notebook).
+ftable(x)       #Summarizes cross tabs for easy analysis.
+object.size(x)  #To know the size of the data.
 
 write.table("table","table.txt",quote = FALSE, sep = "\t")  #Saves a table as text file
 #the quote eliminates quotation marks.
 #sep argument indicates separation using tabulation.
 
 write.xlsx()    #Write an Excel. Requires Library xlsx.
+
+sort(x&"column", decreasing = FALSE, na.last = TRUE)       #It sorts the value of a table using a column as a reference and in an ascending order. You can chose to do it in a descending order and to exclude NAs.
+order(x&"column", x&"column2")           #Reorder the data using a specific column as a reference. If there are repeated values, the column 2 is used to resolve ties.
+
+seq(minimum, maximum, by = "increasing order", length = "how many levels")   #Creates an index from the data using as a reference the argument by and length.
+cut(x$"column", breakes = "How to break the data")         #To create categorical variables.
+
+Hmisc::cut2(x$"column", g = "how to break the data")       #Same as cut but from Package Hmisc.
+
+factor(x$"column", ...)     #To create factor variables. Arguments are to define how to create the factors.
 
                   ------------XML files---------------
 
@@ -125,17 +151,23 @@ ddply(.data = table,        #It gives you the means of column 2 using column 1 a
       .fun = function(x) mean(x$column2)
 )
 
-[which.max(table[,2]),]     #It tells you the highest value taking out the NA.
+[which.max(table[,2]),]     #It tells you the highest value, taking out the NA.
 
 select(dataframe,column1,column2,...)   #Generates a new dataframe selecting specific columns.
 filter(dataframe,row1,row2,...)         #As select but for rows.
 summarize()     #Turns a dataframe in only 1 row.
-group_by()      #Turns a dataframe intro groups depending on specific conditions.
+group_by()      #Turns a dataframe into groups depending on specific conditions.
 is.na()         #Filters NA values.
 na.omit()       #Erase NA rows.
-mutate()        #Add new columns to a dataframe.
+mutate()        #Add new columns to a dataframe. It can be used to do operations using the column data.
 count()         #Counts data.
 %>%             #For pipelines.
+arrange(x, variable)        #Sorting data using a variable (ascending order). Argument desc(var) can be used to do it in descending order.
+x$integer       #To add a new column to a data frame.
+cbind(x, data)  #Binds new columns to the right of the data frame.
+rbind(x, data)  #Binds new rows to the data frame, you can choose to add them at the top or bottom by using arguments.
+Hmisc::mutate(x, "newcolumn" 0 cut2 (column, g = y))            #Create a new variable and add it to the dataset. It needs Package Hmisc.
+rename(dataframe, ...)           #Changes the name of a column.
 
                 -----------------data.table Package----------------
 
@@ -146,6 +178,18 @@ x[,title:"new information"]   #Adding columns to an existing table. You can use 
 x[, .N, by=y]   #.N is a special variable that counts the number of times a value or variable appears in a table.
 fread(file)     #Fast reading tables. Much faster than read.table().
 
+                ----------------reshape2 Package-------------------
+
+melt(x, id = c("columns"), measure.var = c("columns"))            #Organize the data frame according to the content.
+dcast(x, idcolumn ~ measure.varvalue)            #Reorganizes the table using the previous organized content.
+tapply(x$"columntosum", x$"referencecolumn")     #To sum values by a specific variable.
+split(x$"columntouse", x$"referencecolumn")      #Gets a list of values.
+lapply(splitresult, sum)    #Sum of the split values.
+unlist(x)     #To get a vector for data manipulation.
+
+#The same can be done using plyr Package:
+plyr::ddply(x,.(targetcolumn),summarize,sum=sum(columntosum))
+  
                 ---------------------------------------------------
 
 search()        #Shows the search list R is using to assing values to variables and functions.
